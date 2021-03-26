5ac1c944100622cf0355f190071677ac6b2c490c
loaner <loaner@10.1.10.177>
2021-03-25 23:53:41 -0400

    Changes to functionality of frame and display I added functionality to the x button at the top right of the screen I added the logo to the top center of the screen I made it so the calculator opens at a smaller size

--- Cs345 Git/src/gui/MainFrame.java
+++ Cs345 Git/src/gui/MainFrame.java
@@ -1,76 +1,94 @@
 package gui;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.*;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.*;
+
 /**
- * main frame.
+ * MainFrame - .
  * 
  * @author Prof. David Bernstein, James Madison University
  * 
  * @version 1.0
  */
-public class MainFrame extends JFrame   {
-    private static MainFrame frame;
-    private JPanel mainPanel;
-    private MainFrame() {
+public class MainFrame extends JFrame
+{
 
-            createComponents(); // create needed objects
-            setSize( 600, 450 );
-            getContentPane().add( mainPanel, BorderLayout.CENTER );
-            setTitle( "Rimplex" );
-            setVisible( true ); // display this
-            centerForm();
+  private static MainFrame frame;
+  private JPanel mainPanel;
 
-    } // constructor
-        /**
-         * centerForm.
-         *
-         * center form on screen
-         */
-        private void centerForm() {
+  private MainFrame()
+  {
 
-            Dimension dimScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            Dimension dimFrameSize = getSize();
+    createComponents(); // create needed objects
+    setSize(600, 250);
+    getContentPane().add(mainPanel, BorderLayout.CENTER);
+    // getContentPane().add(new JLabel(new ImageIcon("iconRimplex.png")), BorderLayout.NORTH);
+    setTitle("Rimplex");
+    setVisible(true); // display this
+    centerForm(); // center is on the screen to begin
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // make exit button terminate the
+                                                             // Rimplex program
 
-            if ( dimFrameSize.height > dimScreenSize.height ) {
-                dimFrameSize.height = dimScreenSize.height;
-            }
-            if ( dimFrameSize.width > dimScreenSize.width ) {
-                dimFrameSize.width = dimScreenSize.width;
-            }
+  } // constructor
 
-            setLocation( ( dimScreenSize.width - dimFrameSize.width ) / 2,
-                    ( dimScreenSize.height - dimFrameSize.height ) / 2 );
+  /**
+   * Puts the Frame in the center of the screen.
+   * 
+   */
+  private void centerForm()
+  {
 
-        }
+    Dimension dimScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    Dimension dimFrameSize = getSize();
 
-    /**
-     * the panel to be displayed.
-     * simply instantiate to view.
-     */
-    private void createComponents() {
-
-        // create a panel and implement here.
-
-        mainPanel = new MainPanel();
+    if (dimFrameSize.height > dimScreenSize.height)
+    {
+      dimFrameSize.height = dimScreenSize.height;
     }
-        /**
-         * get instance method.
-         *
-         * @return frame
-         */
-        public static MainFrame getInstance() {
+    if (dimFrameSize.width > dimScreenSize.width)
+    {
+      dimFrameSize.width = dimScreenSize.width;
+    }
 
-            if ( frame == null ) {
-                frame = new MainFrame();
+    setLocation((dimScreenSize.width - dimFrameSize.width) / 2,
+        (dimScreenSize.height - dimFrameSize.height) / 2);
 
-            }
+  }
 
-            return frame;
-        }
+  /**
+   * the panel to be displayed. simply instantiate to view.
+   */
+  private void createComponents()
+  {
 
+    // create a panel and implement here.
 
+    mainPanel = new MainPanel();
+  }
+
+  /**
+   * get instance method.
+   *
+   * @return frame the new MainFrame instance
+   */
+  public static MainFrame getInstance()
+  {
+    if (frame == null)
+    {
+      frame = new MainFrame();
+    }
+
+    return frame;
+  }
 
 }
--- Cs345 Git/src/gui/MainPanel.java
+++ Cs345 Git/src/gui/MainPanel.java
@@ -1,98 +1,125 @@
 package gui;
+
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
-import javax.swing.BoxLayout;
 
+/**
+ * MainPanel - .
+ * 
+ * @author Ulises Fernandez, Andrew Elbert
+ * @version (3/31/21)
+ */
 
-public class MainPanel extends JPanel {
-    private CompoundBorder internalBorder;
-    private JButton equalButton;
-    private JButton divButton;
-    private JButton multiButton;
-    private JButton minusButton;
-    private JButton plusButton;
-    private JButton clearButton;
-    private JButton resetButton;
-    private JTextField inputField;
-    private JTextField inputField2;
-    private JLabel label;
+public class MainPanel extends JPanel
+{
+  private CompoundBorder internalBorder;
+  private JButton equalButton;
+  private JButton divButton;
+  private JButton multiButton;
+  private JButton minusButton;
+  private JButton plusButton;
+  private JButton clearButton;
+  private JButton resetButton;
+  private JTextField inputField;
+  private JTextField inputField2;
+  private JLabel label;
+  private JLabel rimpLogo;
 
-    private JPanel northPanel;
-    private JPanel midPanel;
-    private JPanel southPanel;
-    private JPanel temp;
+  private ImageIcon logo;
 
+  private JPanel northPanel;
+  private JPanel midPanel;
+  private JPanel southPanel;
+  private JPanel temp;
 
+  public MainPanel()
+  {
+    createComponents();
+    setParameters();
+    setPanel();
+    addComponents();
+    setListeners();
+  }
 
-    public MainPanel() {
-        createComponents();
-        setParameters();
-        setPanel();
-        addComponents();
-        setListeners();
-    }
-    protected void createComponents() {
+  protected void createComponents()
+  {
 
-        internalBorder = new CompoundBorder( new EmptyBorder( 10, 10, 10, 10 ),
-                new EtchedBorder() );
+    internalBorder = new CompoundBorder(new EmptyBorder(10, 10, 10, 10), new EtchedBorder());
 
-        label = new JLabel("test");
-        inputField = new JTextField();
-        inputField.setSize(2000,10);
-        inputField2 = new JTextField();
-        inputField2.setSize(1000,25);
+    //added this for logo
+    logo = new ImageIcon("logoRimplex.png");
+    rimpLogo = new JLabel(logo);
 
-        // panels
-        northPanel = new JPanel();
-        midPanel = new JPanel();
-        southPanel = new JPanel();
-        temp = new JPanel();
-        // buttons below
-        equalButton = new JButton( "=" );
-        divButton= new JButton( "/" );
-        multiButton = new JButton( "x" );
-        minusButton = new JButton( "-" );
-        plusButton = new JButton( "+" );
+    label = new JLabel("test");
+    inputField = new JTextField();
+    inputField.setSize(2000, 10);
+    inputField2 = new JTextField();
+    inputField2.setSize(1000, 25);
 
-        clearButton = new JButton( "C" );
-        resetButton = new JButton( "R" );
+    // panels
+    northPanel = new JPanel();
+    midPanel = new JPanel();
+    southPanel = new JPanel();
+    temp = new JPanel();
+    // buttons below
+    equalButton = new JButton("=");
+    divButton = new JButton("/");
+    multiButton = new JButton("*");
+    minusButton = new JButton("-");
+    plusButton = new JButton("+");
 
-    } // method createComponents
-    protected void setParameters() {
+    clearButton = new JButton("C");
+    resetButton = new JButton("R");
 
-        midPanel.setBorder( internalBorder );
+  } // method createComponents
 
-    } // method setParameters
-    protected void setPanel() {
+  protected void setParameters()
+  {
 
-        setLayout( new BorderLayout() );
-        midPanel.setLayout( new BoxLayout( midPanel, BoxLayout.Y_AXIS ) );
-        temp.setLayout( new GridLayout(2,1));
-    } // method setPanel
-    protected void addComponents() {
-//        northPanel.add(inputField);
-//        add( northPanel, BorderLayout.NORTH ); // mid panel
+    midPanel.setBorder(internalBorder);
 
-        midPanel.add(inputField2);
-        add( midPanel, BorderLayout.CENTER ); // mid panel
+  } // method setParameters
 
-        southPanel.add( resetButton );
-        southPanel.add( clearButton );
-        southPanel.add( plusButton );
-        southPanel.add( minusButton );
-        southPanel.add( multiButton );
-        southPanel.add( divButton );
-        southPanel.add( equalButton );
-        temp.add(inputField);
-        temp.add(southPanel);
-        add( temp, BorderLayout.SOUTH );
+  protected void setPanel()
+  {
 
-    }
-    protected void setListeners() {
+    setLayout(new BorderLayout());
+    //added this for the logo
+    northPanel.setPreferredSize(new Dimension(logo.getIconWidth(), logo.getIconHeight()));
+    northPanel.setLayout(new BorderLayout());
+    midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
+    temp.setLayout(new GridLayout(2, 1));
+  } // method setPanel
 
-    }
+  protected void addComponents()
+  {
+    // added this for the logo
+    northPanel.add(rimpLogo, BorderLayout.CENTER);
+    add(northPanel, BorderLayout.NORTH); // top panel
+    northPanel.paintComponents(getGraphics());
+
+    midPanel.add(inputField2);
+    add(midPanel, BorderLayout.CENTER); // mid panel
+    
+    southPanel.add(resetButton);
+    southPanel.add(clearButton);
+    southPanel.add(plusButton);
+    southPanel.add(minusButton);
+    southPanel.add(multiButton);
+    southPanel.add(divButton);
+    southPanel.add(equalButton);
+    temp.add(inputField);
+    temp.add(southPanel);
+    add(temp, BorderLayout.SOUTH);
+
+  }
+
+  protected void setListeners()
+  {
+
+  }
 
 }
