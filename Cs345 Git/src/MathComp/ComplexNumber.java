80afc9ba6b8fc762a40f4deea0872b4250cccd07
slack <slack@DESKTOP-SBIRRHT>
2021-03-28 15:48:00 -0400

    complex value is how we store any value that can be in a complex number, the complex number class is just an array of values. value works but number doesnt do anything yet

---
+++ Cs345 Git/src/MathComp/ComplexNumber.java
@@ -0,0 +1,12 @@
+package MathComp;
+
+import java.util.ArrayList;
+
+public class ComplexNumber
+{
+  private ArrayList<ComplexValue> number;
+  
+  public ComplexNumber() {
+    number = new ArrayList<ComplexValue>();
+  }
+}
---
+++ Cs345 Git/src/MathComp/ComplexValue.java
@@ -0,0 +1,117 @@
+package MathComp;
+
+public class ComplexValue
+{
+  private int val;
+  private char car;
+  private char op;
+  private int exp;
+
+  public ComplexValue()
+  {
+  }
+
+  public ComplexValue(int val)
+  {
+    this.val = val;
+    this.car = ' ';
+    this.exp = 1;
+  }
+
+  public ComplexValue(char car)
+  {
+    this.val = 1;
+    this.car = car;
+    this.exp = 1;
+  }
+
+  public ComplexValue(char car, int exp)
+  {
+    this.val = 1;
+    this.car = car;
+    this.exp = exp;
+  }
+
+  public ComplexValue(char car, int exp, int val, char op)
+  {
+    this.val = val;
+    this.car = car;
+    this.exp = exp;
+    this.op = op;
+  }
+
+  public int getVal()
+  {
+    return val;
+  }
+
+  public int getExp()
+  {
+    return exp;
+  }
+
+  public char getOp()
+  {
+    return op;
+  }
+
+  public int getCar()
+  {
+    return car;
+  }
+
+  /*
+   * set methods
+   */
+  public void setVal(int val)
+  {
+    this.val = val;
+  }
+
+  public void setExp(int exp)
+  {
+    this.exp = exp;
+  }
+
+  public void setOp(char op)
+  {
+    this.op = op;
+  }
+
+  public void setCar(char car)
+  {
+    this.car = car;
+  }
+
+  /*
+   * change this to return a new complex value that is a combination of the two values
+   */
+  public ComplexValue add(ComplexValue v)
+  {
+    boolean test = false;
+    ComplexValue ans = null;
+    if (this.car == v.getCar())
+    {
+      ans.setVal(val + v.getVal());
+      if(ans.getVal() == 0) {
+        ans.setCar(' ');
+      }
+    }
+    return ans;
+  }
+  
+  public ComplexValue sub(ComplexValue v)
+  {
+    boolean test = false;
+    ComplexValue ans = null;
+    if (this.car == v.getCar())
+    {
+      ans.setVal(val - v.getVal());
+      if(ans.getVal() == 0) {
+        ans.setCar(' ');
+      }
+    }
+    
+    return ans;
+  }
+}
