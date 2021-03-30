cacd2ee7171af84070304837fc0bf3f26f29a012
Andrew <elbertas@dukes.jmu.edu>
2021-03-30 14:56:58 -0400

    GUI is completely functional except does not call math functions

--- Cs345 Git/src/gui/Listener.java
+++ Cs345 Git/src/gui/Listener.java
@@ -4,71 +4,125 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Listener implements ActionListener {
-private static Listener listener;
-    private Listener() {
+public class Listener implements ActionListener
+{
+  private static Listener listener;
 
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        MainFrame frame = MainFrame.getInstance();
-        
-        Object source = e.getSource();
-        
-        String command = e.getActionCommand();
+  private Listener()
+  {
 
-        /**
-         * 
-        switch (((AbstractButton) source).getText()) {
-            case "=":
-                System.out.println();
-                break;
-            case "/":
-                System.out.println();
+  }
 
-                break;
-            case "*":
-                break;
-            case "-":
-                break;
-            case "+":
-                break;
-            case "C":
-                break;
-            case "R":
-                break;
-            default:
-                System.exit(0);
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    MainFrame frame = MainFrame.getInstance();
+    JTextPane theDis = MainPanel.display;
+    
+    String testAnswer = "The Answer";
+    String previousTotal = "";
+    
+    // Inputs
+    String eqInput;
+    String tInput;
+    String divInput;
+    String multInput;
+    String subInput;
+    String addInput;
+    
+    // Outputs
+    JTextArea output; // eqOutput
+    // JTextArea output; // tOutput
+    // JTextArea output; // divOutput
+    // JTextArea output; // multiOutput
+    // JTextArea output; // subOutput
+    // JTextArea output; // addOutput
+
+    Object source = e.getSource();
+
+    String command = e.getActionCommand();
+
+    switch (command)
+    {
+      case "=":
+        eqInput = MainPanel.getInput().getText();
+        previousTotal.concat(eqInput);
+        // add a line for parsing the text here to pass it to the ComplexNumber or ComplexValue class
+        if (!eqInput.equals(""))
+        {
+          output = MainPanel.getDisplayOutput("(" + eqInput + ") = \n(" + previousTotal + ")\n\n");
+        } else
+        {
+          output = MainPanel.getDisplayOutput("(0) = \n" + previousTotal + ")\n\n");
         }
-        */
-        
-        switch (command) {
-          case "=":
-             String input = ((JTextField) source).getText();
-              System.out.println(input);
-              break;
-          case "/":
-              System.out.println();
-              break;
-          case "*":
-              break;
-          case "-":
-              break;
-          case "+":
-              break;
-          case "C":
-              break;
-          case "R":
-              break;
-          default:
-              System.exit(0);
-      }
+        theDis.insertComponent(output);
+        MainPanel.getInput().setText("");
+        break;
+      case "text":
+        tInput = ((JTextField) source).getText();
+        previousTotal.concat(tInput);
+        // add a line for parsing the text here to pass it to the ComplexNumber or ComplexValue class
+        if (!tInput.equals(""))
+        {
+          output = MainPanel.getDisplayOutput("(" + tInput + ") = \n(" + previousTotal + ")\n\n");
+        } else
+        {
+          output = MainPanel.getDisplayOutput("(0) = \n(" + previousTotal + ")\n\n");
+        }
+        theDis.insertComponent(output);
+        ((JTextField)source).setText("");
+        break;
+      case "/":
+        divInput = MainPanel.getInput().getText();
+        previousTotal.concat(divInput);
+        // add a line for parsing the text here to pass it to the ComplexNumber or ComplexValue class
+        output = MainPanel.getDisplayOutput("(" + divInput + ") / ");
+        theDis.insertComponent(output);
+        MainPanel.getInput().setText("");
+        break;
+      case "*":
+        multInput = MainPanel.getInput().getText();
+        previousTotal.concat(multInput);
+        // add a line for parsing the text here to pass it to the ComplexNumber or ComplexValue class
+        output = MainPanel.getDisplayOutput("(" + multInput + ") * ");
+        theDis.insertComponent(output);
+        MainPanel.getInput().setText("");
+        break;
+      case "-":
+        subInput = MainPanel.getInput().getText();
+        previousTotal.concat(subInput);
+        // add a line for parsing the text here to pass it to the ComplexNumber or ComplexValue class
+        output = MainPanel.getDisplayOutput("(" + subInput + ") - ");
+        theDis.insertComponent(output);
+        MainPanel.getInput().setText("");
+        break;
+      case "+":
+        addInput = MainPanel.getInput().getText();
+        previousTotal.concat(addInput);
+        // add a line for parsing the text here to pass it to the ComplexNumber or ComplexValue class
+        output = MainPanel.getDisplayOutput("(" + addInput + ") + ");
+        theDis.insertComponent(output);
+        MainPanel.getInput().setText("");
+        break;
+      case "C":
+        MainPanel.clear();
+        break;
+      case "R":
+        MainPanel.reset();
+        previousTotal = "";
+        break;
+      default:
+        System.exit(0);
     }
-    public static Listener getInstance(){
-     if ( listener == null ) {
-        listener = new Listener();
+  }
+
+  public static Listener getInstance()
+  {
+    if (listener == null)
+    {
+      listener = new Listener();
     }
 
-        return listener;
-}
+    return listener;
+  }
 }
--- Cs345 Git/src/gui/MainPanel.java
+++ Cs345 Git/src/gui/MainPanel.java
@@ -2,6 +2,8 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
@@ -20,6 +22,8 @@ public class MainPanel extends JPanel
    */
   private static final long serialVersionUID = 1L;
   private CompoundBorder internalBorder;
+  
+  // all buttons
   private JButton equalButton;
   private JButton divButton;
   private JButton multiButton;
@@ -27,17 +31,23 @@ public class MainPanel extends JPanel
   private JButton plusButton;
   private JButton clearButton;
   private JButton resetButton;
-  private JTextField inputField;
-  private JTextField display;
-  // private JLabel label;
-  private JLabel rimpLogo;
+  private ArrayList<JButton> theButtons;
+  
+  // input and output
+  private static JTextField inputField;
+  private static JTextArea output;
+  
+  // making this package visible so that the Listener can access it
+  static JTextPane display;
 
+  // Label and Icon used for displaying the logo on the calculator
+  private JLabel rimpLogo;
   private ImageIcon logo;
 
-  private JPanel northPanel;
-  private JPanel midPanel;
-  private JPanel southPanel;
-  private JPanel temp;
+  private JPanel logoPanel;
+  private JPanel displayPanel;
+  private JPanel inputPanel;
+  private JPanel buttonsAndInput;
 
   public MainPanel()
   {
@@ -45,15 +55,12 @@ public class MainPanel extends JPanel
     setParameters();
     setPanel();
     addComponents();
-    setListeners();
+    setListenersAndCommands();
   }
 
   protected void createComponents()
   {
-
     internalBorder = new CompoundBorder(new EmptyBorder(10, 10, 10, 10), new EtchedBorder());
-    
-    Listener theListener = Listener.getInstance();
 
     // added this for logo
     logo = new ImageIcon("logoRimplex.png");
@@ -62,8 +69,6 @@ public class MainPanel extends JPanel
     // label = new JLabel("test");
     inputField = new JTextField();
     inputField.setSize(2000, 10);
-    inputField.setActionCommand("=");
-    inputField.addActionListener(theListener);
     // when this is called, it will most likely make use of the TextField's getText() method and
     // then for everything that needs to be italicized (if anything), it will put code in which will
     // italicize it and then put that in the display somehow depending on what TextComponent we use
@@ -72,84 +77,113 @@ public class MainPanel extends JPanel
     // and store it as an actual numeric value (or I guess a ComplexNumber), and on the topic of
     // that, we need to figure out what to do when the user enters an invalid expression
 
-    display = new JTextField();
+    display = new JTextPane();
     display.setSize(1000, 25);
     display.setEditable(false);
 
     // panels
-    northPanel = new JPanel();
-    midPanel = new JPanel();
-    southPanel = new JPanel();
-    temp = new JPanel();
+    logoPanel = new JPanel();
+    displayPanel = new JPanel();
+    inputPanel = new JPanel();
+    buttonsAndInput = new JPanel();
 
     // buttons below
     equalButton = new JButton("=");
-    equalButton.setActionCommand("=");
-
     divButton = new JButton("/");
-    divButton.setActionCommand("/");
-
     multiButton = new JButton("*");
-    multiButton.setActionCommand("*");
-
     minusButton = new JButton("-");
-    minusButton.setActionCommand("-");
-
     plusButton = new JButton("+");
-    plusButton.setActionCommand("+");
-
     clearButton = new JButton("C");
-    clearButton.setActionCommand("C");
-
     resetButton = new JButton("R");
-    resetButton.setActionCommand("R");
+    
+    theButtons = new ArrayList<JButton>();
+    theButtons.add(clearButton);
+    theButtons.add(equalButton);
+    theButtons.add(resetButton);
+    theButtons.add(divButton);
+    theButtons.add(minusButton);
+    theButtons.add(multiButton);
+    theButtons.add(plusButton);
 
   } // method createComponents
 
   protected void setParameters()
   {
 
-    midPanel.setBorder(internalBorder);
+    displayPanel.setBorder(internalBorder);
 
   } // method setParameters
 
   protected void setPanel()
   {
-
     setLayout(new BorderLayout());
     // added this for the logo
-    northPanel.setPreferredSize(new Dimension(logo.getIconWidth(), logo.getIconHeight()));
-    northPanel.setLayout(new BorderLayout());
-    midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
-    temp.setLayout(new GridLayout(2, 1));
+    logoPanel.setPreferredSize(new Dimension(logo.getIconWidth(), logo.getIconHeight()));
+    logoPanel.setLayout(new BorderLayout());
+    displayPanel.setLayout(new BoxLayout(displayPanel, BoxLayout.Y_AXIS));
+    buttonsAndInput.setLayout(new GridLayout(2, 1));
   } // method setPanel
 
+  /**
+   * Adds all Panels to this MainPanel.
+   */
   protected void addComponents()
   {
     // added this for the logo
-    northPanel.add(rimpLogo, BorderLayout.CENTER);
-    add(northPanel, BorderLayout.NORTH); // top panel
-    northPanel.paintComponents(getGraphics());
+    logoPanel.add(rimpLogo, BorderLayout.CENTER);
+    add(logoPanel, BorderLayout.NORTH); // top panel
+    logoPanel.paintComponents(getGraphics());
 
-    midPanel.add(display);
-    add(midPanel, BorderLayout.CENTER); // mid panel
+    displayPanel.add(display);
+    add(displayPanel, BorderLayout.CENTER); // mid panel
 
-    southPanel.add(resetButton);
-    southPanel.add(clearButton);
-    southPanel.add(plusButton);
-    southPanel.add(minusButton);
-    southPanel.add(multiButton);
-    southPanel.add(divButton);
-    southPanel.add(equalButton);
-    temp.add(inputField);
-    temp.add(southPanel);
-    add(temp, BorderLayout.SOUTH);
+    inputPanel.add(resetButton);
+    inputPanel.add(clearButton);
+    inputPanel.add(plusButton);
+    inputPanel.add(minusButton);
+    inputPanel.add(multiButton);
+    inputPanel.add(divButton);
+    inputPanel.add(equalButton);
+    buttonsAndInput.add(inputField);
+    buttonsAndInput.add(inputPanel);
+    add(buttonsAndInput, BorderLayout.SOUTH);
 
   }
 
-  protected void setListeners()
+  /**
+   * Adds theListener to the inputField and all Buttons
+   */
+  protected void setListenersAndCommands()
   {
-
+    Listener theListener = Listener.getInstance();
+    for (JButton button : theButtons) {
+      button.addActionListener(theListener);
+      button.setActionCommand(button.getText());
+    }
+    inputField.setActionCommand("text");
+    inputField.addActionListener(theListener);
+  }
+  
+  static JTextArea getDisplayOutput(String text) {
+    if (output == null) {
+      output = new JTextArea();
+      output.setEditable(false);
+    }
+    output.append(text);
+    return output;
+  }
+  
+  static void clear() {
+    inputField.setText("");
+  }
+  
+  static void reset() {
+    clear();
+    display.setText("");
+  }
+  
+  static JTextField getInput() {
+    return inputField;
   }
 
 }
