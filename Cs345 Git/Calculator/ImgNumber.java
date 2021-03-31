4ff1a722190ba46f23c785759026d7161d53f358
slack <slack@DESKTOP-SBIRRHT>
2021-03-31 01:45:34 -0400

    everything works just needs to be formated

--- Cs345 Git/Calculator/ImgNumber.java
+++ Cs345 Git/Calculator/ImgNumber.java
@@ -1,8 +1,18 @@
 package Calculator;
 
+import MathComp.Operator;
+
 public class ImgNumber
 {
   private final double real, img;
+  private Operator operator;
+
+  public ImgNumber(double real, double img, Operator operator)
+  {
+    this.real = real;
+    this.img = img;
+    this.operator = operator;
+  }
 
   public ImgNumber(double real, double img)
   {
@@ -20,6 +30,14 @@ public class ImgNumber
     return img;
   }
 
+  /**
+   * @return the operator in the expression
+   */
+  public Operator getOperator()
+  {
+    return operator;
+  }
+
   public ImgNumber add(ImgNumber other)
   {
     double newReal = this.real + other.getReal();
@@ -34,15 +52,77 @@ public class ImgNumber
 
   public ImgNumber multiply(final ImgNumber other)
   {
-    double newReal = this.real * other.getReal() - this.img + other.getImg();
-    double newImg = this.real * other.getImg() - this.img + other.getReal();
+    double newReal = this.real * other.getReal() - this.img * other.getImg();
+    double newImg = this.real * other.getImg() + this.img * other.getReal();
     return new ImgNumber(newReal, newImg);
   }
 
   public ImgNumber divide(final ImgNumber other)
   {
-    double newReal;
-    double newImg;
-    return other;
+    return this.multiply(other.reciprocal());
   }
+
+  /**
+   * helper method for getting reciprocal.
+   * 
+   * @return
+   */
+  public ImgNumber reciprocal()
+  {
+    double rec = real * real + img * img;
+    return new ImgNumber((real / rec), (-img / rec));
+  }
+
+  /*
+   * @return true if
+   * 
+   * the ImgNumber has no value
+   */
+
+  private boolean hasNoValue()
+  {
+    if (real == 0 && img == 0)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  /*
+   * 
+   * Returns a String representation of a complex value.
+   */
+
+  public String toString()
+  {
+    String result = "";
+
+    if (hasNoValue())
+    {
+      return Double.toString(0);
+    }
+
+    // no real part
+    if (real == 0)
+    {
+      result += img + "i";
+    }
+    // no imaginary part
+    else if (img == 0)
+    {
+      result += real + "";
+    }
+    // negative imaginary value
+    else if (img < 0)
+    {
+      // cancel the negative when formatting
+      result += real + " " + Operator.SUBTRACT.getOperator() + " " + -img + "i";
+    }
+    else
+    {
+      result += real + " " + Operator.ADD.getOperator() + " " + img + "i";
+    }
+    return result;
+  }
+
 }
