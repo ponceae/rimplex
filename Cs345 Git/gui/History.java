580961ebb66de6d7e354d93eae36d5fa7067e8a4
fernandez-u <80287996+fernandez-u@users.noreply.github.com>
2021-04-13 23:34:27 -0400

    Create History.java
    Store Inputs in history class.

---
+++ Cs345 Git/gui/History.java
@@ -0,0 +1,26 @@
+package gui;
+import java.util.ArrayList;
+
+public class History {
+    private ArrayList<String> history;
+
+    public History(){
+        history = new ArrayList<>();
+    }
+    public void add(String str){
+        history.add(str);
+    }
+    public void reset() {
+        history = new ArrayList<>();
+    }
+    public String toString() {
+        if(history == null || history.size() == 0){
+            return "";
+        }
+        String str = "";
+        for(String s:history) {
+            str += s +"\n";
+        }
+        return str;
+    }
+}
