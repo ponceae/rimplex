ec23c255cc139059ac9bc0d6c3dbf0c4219786e1
fernandez-u <80287996+fernandez-u@users.noreply.github.com>
2021-04-23 16:04:01 -0400

    Create mouse listener
    mouse pressed method

---
+++ Cs345 Git/gui/MouseList.java
@@ -0,0 +1,31 @@
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseList implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        PopUp.infoBox("Info","About");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
