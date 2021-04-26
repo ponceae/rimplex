31c7cfcf7026508a89a3cfa666a9585fa1e89fe7
fernandez-u <80287996+fernandez-u@users.noreply.github.com>
2021-04-26 14:45:39 -0400

    Update class with menu functions
    Added functions to menu and help.

--- Cs345 Git/gui/MouseList.java
+++ Cs345 Git/gui/MouseList.java
@@ -1,7 +1,13 @@
 package gui;
 
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MouseList implements MouseListener {
 
@@ -12,7 +18,31 @@ public class MouseList implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        PopUp.infoBox("Info","About");
+        JMenu menu = (JMenu) e.getSource();
+        String name = menu.getText();
+        switch(name) {
+            case "About":
+                PopUp.infoBox("Info","About");
+                break;
+            case "Help":
+                try {
+                    getHelp();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } catch (URISyntaxException uriSyntaxException) {
+                    uriSyntaxException.printStackTrace();
+                }
+                break;
+            case "English":
+                break;
+            case "French":
+                break;
+            case "German":
+                break;
+            default:
+                break;
+        }
+
     }
 
     @Override
@@ -28,5 +58,12 @@ public class MouseList implements MouseListener {
     @Override
     public void mouseExited(MouseEvent e) {
 
+    }
+    
+     private void getHelp() throws IOException, URISyntaxException {
+        Desktop desktop = Desktop.getDesktop();
+         // can be changed to any address
+        URI     uri     = new URI("https://www.google.com/webhp?hl=en&ictx=2&sa=X&ved=0ahUKEwjW67yRyJzwAhViUd8KHRncCjoQPQgI");
+        desktop.browse(uri);
     }
 }
